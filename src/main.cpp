24857e73cf320bf6934094289dbd8ba2ade99495
kingweicai <kingweicai@tencent.com>
2018-04-19 13:13:42 +0800

    remove some useless code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "GifEncoder.h"
 #include <vector>
@@ -55,8 +54,6 @@ class RGBAToGIFWorker : public Nan::AsyncWorker {
     void HandleOKCallback () {
         
         v8::Local<v8::Object> gifBuffer = Nan::NewBuffer((char *)(gifEncoder->out->data()), gifEncoder->out->size(), buffer_delete_callback, gifEncoder->out).ToLocalChecked();
-        // v8::Local<v8::Object> gifBuffer = Nan::NewBuffer((char *)((gifEncoder->out).data()), (gifEncoder->out).size()).ToLocalChecked();
-        // v8::Local<v8::Object> gifBuffer = Nan::CopyBuffer((char *)((gifEncoder->out).data()), (gifEncoder->out).size()).ToLocalChecked();
         v8::Local<v8::Value> argv[] = { Nan::Null(), gifBuffer };
         callback->Call(2, argv);
     }
@@ -79,72 +76,6 @@ class RGBAToGIFWorker : public Nan::AsyncWorker {
 };
 
 
-
-// /*
-// 	delay, repeat, buffers, callback
-// */
-// void picsToGIF(const v8::FunctionCallbackInfo<Value>& args) {
-
-// 	Isolate* isolate = args.GetIsolate();
-
-//     if (args.Length() != 4) {
-//         isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments, expected 4 arguments!")));
-//         return;
-//     }
-//     if (!args[0]->IsNumber()) {
-//       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, delay should be a number!")));
-//       return;
-//     }
-//     if (!args[1]->IsNumber()) {
-//       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, repeat should be a number!")));
-//       return;
-//     }
-
-//     /*judge array of buffer*/
-//     // if(!args[2]->()){
-//     //     isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, buffers should a Buffers type!")));
-//     //     return;
-//     // }
-
-//     if(!args[3]->IsFunction()){
-//         isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, callback should be a function!")));
-//         return;
-//     }
-
-// 	int delay = args[0]->Int32Value();
-//     int repeat = args[1]->Int32Value();
-
-// 	Local<Array> array = Local<Array>::Cast(args[2]);
-
-// 	Nan::Callback *callback = new Nan::Callback( v8::Local<v8::Function>::Cast(args[3]));
-
-// 	std::vector<unsigned char *> buffersVec;
-
-//     int imgWidth = 0,
-//         imgHeight = 0,
-//         imgChannels = 0,
-//         desiredChannels = 0,
-//         rowDataLen = 0;
-
-// 	for (unsigned int i = 0; i < array->Length(); i++ ) {
-
-
-
-//         unsigned char *rowData = (unsigned char *)node::Buffer::Data( array->Get(i)->ToObject() );
-
-//         rowDataLen = node::Buffer::Length(array->Get(i)->ToObject());
-
-// 		unsigned char *pixelData = stbi_load_from_memory(rowData, rowDataLen, &imgWidth, &imgHeight, &imgChannels, desiredChannels);
-
-// 		buffersVec.push_back( pixelData );
-// 	}
-
-// 	Nan::AsyncQueueWorker(new RGBAToGIFWorker(callback, buffersVec, imgWidth, imgHeight, imgChannels, delay, repeat));
-
-// }
-
-
-
 /*
     delay, repeat, buffers, callback
 */
@@ -153,19 +84,15 @@ NAN_METHOD(picsToGIF) {
 
     if (info.Length() != 4) {
         Nan::ThrowTypeError("Wrong number of arguments, expected 4 arguments!");
-
-        // isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments, expected 4 arguments!")));
         return;
     }
     if (!info[0]->IsNumber()) {
         Nan::ThrowTypeError("Wrong arguments, delay should be a number!");
-        // isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, delay should be a number!")));
-      return;
+        return;
     }
     if (!info[1]->IsNumber()) {
         Nan::ThrowTypeError("Wrong arguments, repeat should be a number!");
-        // isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, repeat should be a number!")));
-      return;
+        return;
     }
 
     /*judge array of buffer*/
@@ -176,7 +103,6 @@ NAN_METHOD(picsToGIF) {
 
     if(!info[3]->IsFunction()){
         Nan::ThrowTypeError("Wrong arguments, callback should be a function!");
-        // isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments, callback should be a function!")));
         return;
     }
 
@@ -209,23 +135,6 @@ NAN_METHOD(picsToGIF) {
     Nan::AsyncQueueWorker(new RGBAToGIFWorker(callback, buffersVec, imgWidth, imgHeight, imgChannels, delay, repeat));
 
 }
-
-
-
-
-
-
-// void init(Local<Object> exports) {
-
-//   NODE_SET_METHOD(exports, "picsToGIF", picsToGIF);
-
-
-
-// }
-
-// NODE_MODULE(NODE_GYP_MODULE_NAME, init)
-
-
 
 
 NAN_MODULE_INIT(Init) {
